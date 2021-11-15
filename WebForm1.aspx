aa541eebc567279425d10a0f62638fd6ada3ca33
milin <milin@DESKTOP-I08T1FK>
2021-11-15 18:50:13 +0530

    Add project files.

---
+++ WebForm1.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Task1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="DropDownList1"  runat="server">
+                <asp:ListItem Text="Select" Value="-1"></asp:ListItem>
+            </asp:DropDownList><br /><br />
+            <asp:RequiredFieldValidator   ID="RequiredFieldValidator1"   ForeColor="red" runat="server" ControlToValidate="DropDownList1" InitialValue="1" ErrorMessage="Please Select The Department">
+            </asp:RequiredFieldValidator><br />
+            
+            <asp:Button ID="Button1" runat="server" Text="Save" OnClick="Button1_Click" />
+            
+        </div>
+        <p>
+            <asp:Label ID="Label1" runat="server"></asp:Label>
+            
+        </p>
+        <asp:ValidationSummary ID="ValidationSummary1" runat="server" />
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data.Entity.SqlServer;
+
+
+
+namespace Task1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+                string CS = ConfigurationManager.ConnectionStrings["SampleDBEntities"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(CS))
+                {
+                    SqlCommand cmd = new SqlCommand("Select Id, Department from tblTask2", con);
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    DropDownList1.DataTextField = "Department";
+                    DropDownList1.DataValueField = "Id";
+                    DropDownList1.DataSource = rdr;
+                    DropDownList1.DataBind();
+                }
+            }
+
+        }
+
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+
+            string CS = ConfigurationManager.ConnectionStrings["SampleDBEntities"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand("spFetchData", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParamId = new SqlParameter();
+                {
+                    ParamId.ParameterName = "@Id";
+                    ParamId.Value = Convert.ToInt32(DropDownList1.SelectedValue.ToString());
+
+                }
+                    cmd.Parameters.Add(ParamId);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+
+               
+
+                Label1.Text = DropDownList1.SelectedItem.Text;
+            }
+        }
+    }
+}
